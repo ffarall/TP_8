8c831b6f9190ff6f73032e96e393510fc4d345d0
facundofarall <33322493+facundofarall@users.noreply.github.com>
2018-10-04 13:36:38 -0300

    mergeando con santi

--- TP_8/TP_8/Compressor.cpp
+++ TP_8/TP_8/Compressor.cpp
@@ -1,12 +1,28 @@
 #include "Compressor.h"
+#include <cmath>
 
 
 
-Compressor::Compressor()
+Compressor::Compressor() : pixelMatrix()
 {
+	n = 0;
+}
+
+Compressor::Compressor(char * dataArray, int n_) : pixelMatrix()
+{
+	init(dataArray, n_);
 }
 
 
 Compressor::~Compressor()
 {
+}
+
+void Compressor::init(char * dataArray, int n_)
+{
+	n = n_;
+	for (int i = 0; i < n * n; i++)
+	{
+		pixelMatrix.push_back(Pixel(*(dataArray + i + 1), *(dataArray + i + 2), *(dataArray + i + 3), *(dataArray + i + 4)));	// Add n*n Pixels initialised with (red, green, blue, alfa).
+	}
 }
--- TP_8/TP_8/Compressor.h
+++ TP_8/TP_8/Compressor.h
@@ -10,11 +10,16 @@ class Compressor
 {
 public:
 	Compressor();
-	Compressor(char * dataArray);
+	Compressor(char * dataArray, int n_);
 	~Compressor();
+
+	void init(char * dataArray, int n_);
+	bool encode();					// Creates compressed file from the pixelMatrix.
+	bool decode(char * filename);	// Creates .png file from compressed file.
 
 private:
 	vector<Pixel> pixelMatrix;		// Matrix containing n*n Pixels.
 	int n;							// Pixels per side in matrix.
+	Pixel getPixel(int x, int y);	// Method to treat pixelMatrix as a matrix (kind of implementing pointer's algebra).
 };
 
