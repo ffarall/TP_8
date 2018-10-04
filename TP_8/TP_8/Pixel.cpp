7280e87d93973ce54b02ea06a234804e2066d83e
Santiago Arribere <sarribere@itba.edu.ar>
2018-10-04 13:00:34 -0300

    pixel

--- TP_8/TP_8/Pixel.cpp
+++ TP_8/TP_8/Pixel.cpp
@@ -4,6 +4,35 @@
 
 Pixel::Pixel()
 {
+	R = G = B = alpha = 0;
+}
+
+Pixel::Pixel(const char * content)
+{
+	R = *content;
+	G = *(content+1);
+	B = *(content+2);
+	alpha = *(content+3);
+}
+
+unsigned char Pixel::getR(void)
+{
+	return R;
+}
+
+unsigned char Pixel::getG(void)
+{
+	return G;
+}
+
+unsigned char Pixel::getB(void)
+{
+	return B;
+}
+
+unsigned char Pixel::getAlpha(void)
+{
+	return alpha;
 }
 
 
--- TP_8/TP_8/Pixel.h
+++ TP_8/TP_8/Pixel.h
@@ -3,6 +3,20 @@ class Pixel
 {
 public:
 	Pixel();
+	Pixel(const char * content);
+
+	unsigned char getR(void);
+	unsigned char getG(void);
+	unsigned char getB(void);
+	unsigned char getAlpha(void);
+	
 	~Pixel();
+
+private:
+	unsigned char R;
+	unsigned char G;
+	unsigned char B;
+	unsigned char alpha;
+
 };
 
